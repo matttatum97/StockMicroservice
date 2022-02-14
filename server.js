a49b9480cfb7ff2dceaad288cf8876a7c2d95b9e
matttatum97 <69768161+matttatum97@users.noreply.github.com>
2022-02-14 09:05:46 -0500

    changed cors origin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const investedRouter = require("./routes/investRouter.js");
 const { options } = require("./routes/stockRouter.js");
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({origin:"http://localhost:4200", optionsSuccessStatus:200, credentials:true}))
+app.use(cors({origin:"http://site.kitemutual.online", optionsSuccessStatus:200, credentials:true}))
 app.use(express.json()) //For JSON requests
 app.use(express.urlencoded({extended: true}));
 app.use("/invest", investedRouter)
